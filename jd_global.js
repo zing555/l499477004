b5df10aab3ca9f6bdf9a485dee012b13cb5dea32
l499477004 <35189123+l499477004@users.noreply.github.com>
2021-02-19 16:55:55 +0800

    update

--- jd_global.js
+++ jd_global.js
@@ -321,7 +321,7 @@ function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
     $.get({
-      url: `http://jd.turinglabs.net/api/v2/jd/jdglobal/read/${randomCount}/`,
+      url: `https://raw.githubusercontent.com/l499477004/updateteam/master/jd_globalShareCode.json`,
       'timeout': 10000
     }, (err, resp, data) => {
       try {
